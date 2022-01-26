132767a8427b2b38091da2dcaf4d64c8b8c431c8
David Galvis <>
2022-01-26 10:07:49 -0500

    interfaces and multiple levels of implementatios

--- js/Interfaces.ts
+++ js/Interfaces.ts
@@ -1,10 +1,17 @@
-interface IPerson {
-    name: string;
+interface IName {
+    //We can have constants as well in interfaces
+    readonly name: string;
+}
+
+//we can extend multiple interfaces as well
+interface IPerson  extends IName {
     age: number;
     occupation: string;
     hello(phrases: string):void;
 }
 
+//we can extend multiple interfaces as well but we will need to implement both of them, but we can extend only one class
+// class Person implements IPerson, IName {
 class Person implements IPerson {
 
     constructor(public name: string, public age: number, public occupation: string){
